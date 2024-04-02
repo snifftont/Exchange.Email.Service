61607f2eb51881234a74c8a8bb28f7b460a53bc1
Sniffton Technologies (P) Ltd <63076348+snifftont@users.noreply.github.com>
2024-04-02 23:41:39 +0530

    Add files via upload

---
+++ Service1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace emailService
+{
+    partial class emailService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.email_timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // email_timer
+            // 
+            this.email_timer.Enabled = true;
+            this.email_timer.Interval = 300000;
+            this.email_timer.Tick += new System.EventHandler(this.email_timer_Tick);
+            // 
+            // emailService
+            // 
+            this.ServiceName = "emailService";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.Timer email_timer;
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Net;
+using System.Configuration;
+using System.IO;
+
+namespace emailService
+{
+    public partial class emailService : ServiceBase
+    {
+        public emailService()
+        {
+            InitializeComponent();
+              
+    //if(!System.Diagnostics.EventLog.SourceExists("emailServiceLog"))
+    //    System.Diagnostics.EventLog.CreateEventSource("emailServiceLog", "emailLog");
+
+    //eventLog1.Source = "emailServiceLog";
+    //// the event log source by which 
+
+    ////the application is registered on the computer
+
+    //eventLog1.Log = "emailLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            email_timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            email_timer.Stop();
+        }
+
+        private void email_timer_Tick(object sender, EventArgs e)
+        {
+            send_rquest();
+           
+        }
+       
+        private bool send_rquest()
+        {
+            try
+            {
+                
+                
+                string uri = System.Configuration.ConfigurationSettings.AppSettings["root_url"].ToString() + "/dailyemail.aspx?myid=1";
+                //HttpWebRequest webrequest = (HttpWebRequest)System.Net.WebRequest.Create(uri);
+                //HttpWebResponse webresponse = (HttpWebResponse)webrequest.GetResponse();
+                //webresponse.Close();
+                WebClient webClient = new WebClient();
+                Stream stream = webClient.OpenRead(uri);
+                stream.Close();
+                webClient.Dispose();
+            }
+            catch (Exception ex)
+            {
+            }
+            return true;
+
+        }
+        private bool send_rquest2()
+        {
+            try
+            {
+                string uri = System.Configuration.ConfigurationSettings.AppSettings["root_url"].ToString() + "/spam_proc.aspx?myid=1";
+                HttpWebRequest webrequest = (HttpWebRequest)System.Net.WebRequest.Create(uri);
+                HttpWebResponse webresponse = (HttpWebResponse)webrequest.GetResponse();
+                webresponse.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            return true;
+        }
+    }
+}
